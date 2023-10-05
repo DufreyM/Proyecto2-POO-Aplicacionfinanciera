0ad46f996772f087c1e8f5942d50a669fd19b7e3
GenserCatalan <gensercatalanandre@gmail.com>
2023-10-04 21:36:12 -0600

    Clase Educacion Financiera
    Se creo la clase de educación financiera

---
+++ EducacionFinanciera.java
@@ -0,0 +1,29 @@
+public class EducacionFinanciera {
+
+    private String Planificación_Financiera_Personal;
+    private String Ahorro_Inversion;
+    private String Gestion_deudas;
+    private String subtema4;
+    private String subtema5;
+    private String subtema6;
+
+    public EducacionFinanciera() {
+        Planificación_Financiera_Personal = 
+        "La planificación financiera personal es básicamente un plan o un conjunto de pasos que sigues para administrar tu dinero de manera inteligente.\n" +
+        "\n¿Por qué es importante?\n" +
+        "Bueno, porque te ayuda a alcanzar tus metas financieras a largo plazo, como comprar un auto, ir a la universidad, o incluso retirarte cómodamente cuando seas mayor.";
+    
+        Ahorro_Inversion =
+        "El ahorro es guardar una parte de tu dinero en lugar de gastarlo todo.\n" +
+        "Esto te permite tener dinero extra para emergencias o para comprar cosas que realmente deseas en el futuro.\n" +
+        "\nLa inversión es cuando tomas tu dinero y lo pones en lugares donde puede crecer con el tiempo, como acciones, fondos mutuos o bienes raíces.\n" +
+        "Con la inversión, tu dinero puede aumentar en valor y ayudarte a alcanzar metas financieras más grandes a largo plazo.";
+
+        Gestion_deudas =
+        "";
+        
+        }
+
+
+    }
+    
